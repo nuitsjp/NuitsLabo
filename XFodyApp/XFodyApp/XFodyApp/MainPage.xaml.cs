0c631432d31a358c4a1ee2d1b278dfe685f492da
nuits.jp <nuits.jp@live.jp>
2017-12-06 15:18:45 +0900

    Apply Cauldron.Interception.

--- XFodyApp/XFodyApp/XFodyApp/MainPage.xaml.cs
+++ XFodyApp/XFodyApp/XFodyApp/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,14 @@ namespace XFodyApp
 		public MainPage()
 		{
 			InitializeComponent();
+		    Hoge("Hello, Interceptor");
 		}
+
+	    [Interceptor("Any valid attribute parameter types")]
+        public int Hoge(string message)
+	    {
+	        Debug.WriteLine(message);
+	        return -1;
+	    }
 	}
 }
