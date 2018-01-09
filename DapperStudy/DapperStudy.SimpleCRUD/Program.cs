9ce6fd259dd010c94d3e6af87283b668231638f5
nuits.jp <nuits.jp@live.jp>
2018-01-09 17:49:29 +0900

    Add DapperStudy

---
+++ DapperStudy/DapperStudy.SimpleCRUD/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperStudy.SimpleCRUD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var connection = new SqlConnection(GetConnectionString()))
+            {
+                connection.Open();
+            }
+        }
+
+        private static string GetConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["AdventureWorks2017"].ConnectionString;
+        }
+    }
+}
