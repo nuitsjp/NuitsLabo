daf3da39a26f5eb1e5b66ad1d994577b996b7c22
Atsushi Nakamura <nuits.jp@live.jp>
2019-06-20 06:26:09 +0900

    Add WindowsServiceStudy.

---
+++ WindowsServiceStudy/WindowsServiceStudy/FileWriterService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace WindowsServiceStudy
+{
+    public class FileWriterService : IHostedService, IDisposable
+    {
+        private Timer _timer;
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            WriteLog();
+            _timer = new Timer(
+                (e) => WriteTimeToFile(),
+                null,
+                TimeSpan.Zero,
+                TimeSpan.FromSeconds(1));
+
+            return Task.CompletedTask;
+        }
+
+        public void WriteTimeToFile()
+        {
+            WriteLog();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            WriteLog();
+            _timer?.Change(Timeout.Infinite, 0);
+
+            return Task.CompletedTask;
+        }
+
+        private void WriteLog([CallerMemberName] string memberName = null)
+        {
+            Debug.WriteLine(memberName);
+            Console.WriteLine(memberName);
+        }
+
+        public void Dispose()
+        {
+            _timer?.Dispose();
+        }
+    }
+}
---
+++ WindowsServiceStudy/WindowsServiceStudy/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace WindowsServiceStudy
+{
+    class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            var isService = !(Debugger.IsAttached || args.Contains("--console"));
+
+            var builder = new HostBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<FileWriterService>();
+                });
+
+            if (isService)
+            {
+                await builder.RunAsServiceAsync();
+            }
+            else
+            {
+                await builder.RunConsoleAsync();
+            }
+        }
+    }
+}
