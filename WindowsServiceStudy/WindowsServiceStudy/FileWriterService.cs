c54d64573bb80dbbcf476f9670263f01c86d8970
Atsushi Nakamura <nuits.jp@live.jp>
2019-06-20 06:53:41 +0900

    Add Comment.

--- WindowsServiceStudy/WindowsServiceStudy/FileWriterService.cs
+++ WindowsServiceStudy/WindowsServiceStudy/FileWriterService.cs
@@ -10,8 +10,16 @@ namespace WindowsServiceStudy
 {
     public class FileWriterService : IHostedService, IDisposable
     {
+        /// <summary>
+        /// 定期実行処理を行うTimerオブジェクト
+        /// </summary>
         private Timer _timer;
 
+        /// <summary>
+        /// Windowsサービスを開始する
+        /// </summary>
+        /// <param name="cancellationToken">開始処理自体が重い場合に、キャンセルを受け付けるためのトークン。起動処理が重くないなら使用しなくてよい。</param>
+        /// <returns></returns>
         public Task StartAsync(CancellationToken cancellationToken)
         {
             WriteLog();
@@ -24,14 +32,23 @@ namespace WindowsServiceStudy
             return Task.CompletedTask;
         }
 
+        /// <summary>
+        /// 実際のWindowsサービスの実体
+        /// </summary>
         public void WriteTimeToFile()
         {
             WriteLog();
         }
 
+        /// <summary>
+        /// Windowsサービスの停止処理
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
         public Task StopAsync(CancellationToken cancellationToken)
         {
             WriteLog();
+            // Timerの次の開始時刻を無限つまり実質停止させる
             _timer?.Change(Timeout.Infinite, 0);
 
             return Task.CompletedTask;
