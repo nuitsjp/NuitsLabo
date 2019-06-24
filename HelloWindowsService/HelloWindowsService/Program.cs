86373a1929997ca1fbb545b54052c90219992d5e
Atsushi Nakamura <nuits.jp@live.jp>
2019-06-25 06:38:38 +0900

    Add HelloWindowsService project.

---
+++ HelloWindowsService/HelloWindowsService/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+namespace HelloWindowsService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class LoggingService : IHostedService, IDisposable
+    {
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
