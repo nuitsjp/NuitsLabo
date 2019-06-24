0d0cc22e3d210401569c71324fa1221c6736e0a6
Atsushi Nakamura <nuits.jp@live.jp>
2019-06-25 07:12:19 +0900

    Update HelloWindowsService

--- HelloWindowsService/HelloWindowsService/Program.cs
+++ HelloWindowsService/HelloWindowsService/Program.cs
@@ -1,34 +1,60 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace HelloWindowsService
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var builder = new HostBuilder()
+                .ConfigureServices((hostContext, services) => { services.AddHostedService<LoggingService>(); });
+            await builder.RunConsoleAsync();
         }
     }
 
     public class LoggingService : IHostedService, IDisposable
     {
+        private Timer Timer { get; set; }
 
         public Task StartAsync(CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            Logging();
+            Timer = new Timer(
+                (e) => Running(),
+                null,
+                TimeSpan.Zero,
+                TimeSpan.FromSeconds(1));
+
+            return Task.CompletedTask;
+        }
+
+        private void Running()
+        {
+            Logging();
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            Logging();
+            Timer?.Change(Timeout.Infinite, 0);
+
+            return Task.CompletedTask;
         }
 
         public void Dispose()
         {
-            throw new NotImplementedException();
+            Timer?.Dispose();
+        }
+
+        private void Logging([CallerMemberName] string memberName = null)
+        {
+            File.AppendAllText("log.txt", $"{DateTime.Now} - {memberName}{Environment.NewLine}");
         }
     }
 }
