68b0cdfd7cdd94319c481e89c6b6d2b0a24150d4
Atsushi Nakamura <nuits.jp@live.jp>
2019-12-29 08:36:34 +0900

    add DockerDotNet

---
+++ DockerDotNet/DockerDotNet/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Docker.DotNet;
+using Docker.DotNet.Models;
+
+namespace DockerDotNet
+{
+    class Program
+    {
+        const string ImageName = "nginx";
+        const string Tag = "latest";
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("create docker client.");
+            var dockerApiUri = GetDockerApiUri();
+            var dockerClient = 
+                new DockerClientConfiguration(dockerApiUri).CreateClient();
+
+            Console.WriteLine("pull image...");
+            await PullImage(dockerClient);
+
+            Console.WriteLine("start container.");
+            var containerId = await StartContainerAsync(dockerClient);
+
+            Console.WriteLine("Open http://localhost:8080 in your browser. And Press Enter.");
+            Console.ReadLine();
+
+            Console.WriteLine("stop container.");
+            await StopContainerAsync(dockerClient, containerId);
+        }
+
+        private static async Task PullImage(DockerClient dockerClient)
+        {
+            await dockerClient.Images
+                .CreateImageAsync(new ImagesCreateParameters
+                    {
+                        FromImage = ImageName,
+                        Tag = Tag
+                    },
+                    new AuthConfig(),
+                    new Progress<JSONMessage>());
+        }
+
+        private static async Task<string> StartContainerAsync(DockerClient dockerClient)
+        {
+            var response = await dockerClient.Containers.CreateContainerAsync(new CreateContainerParameters
+            {
+                Image = ImageName,
+                ExposedPorts = new Dictionary<string, EmptyStruct>
+                {
+                    {
+                        "80", default(EmptyStruct)
+                    }
+                },
+                HostConfig = new HostConfig
+                {
+                    PortBindings = new Dictionary<string, IList<PortBinding>>
+                    {
+                        {"80", new List<PortBinding> {new PortBinding {HostPort = "8080" } }}
+                    },
+                    PublishAllPorts = true
+                }
+            });
+            await dockerClient.Containers.StartContainerAsync(response.ID, null);
+            return response.ID;
+        }
+
+        private static async Task StopContainerAsync(DockerClient dockerClient, string containerId)
+        {
+            await dockerClient.Containers.KillContainerAsync(containerId, new ContainerKillParameters());
+        }
+
+
+        private static Uri GetDockerApiUri()
+        {
+            var isWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+
+            if (isWindows)
+            {
+                return new Uri("npipe://./pipe/docker_engine");
+            }
+
+            var isLinux = RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+
+            if (isLinux)
+            {
+                return new Uri("unix:/var/run/docker.sock");
+            }
+
+            throw new Exception("Was unable to determine what OS this is running on, does not appear to be Windows or Linux!?");
+        }
+    }
+}
---
+++ RxCombineSample/RxCombineSample/MainWindowViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Reactive.Bindings;
+
+namespace RxCombineSample
+{
+    public class MainWindowViewModel
+    {
+        public ReactiveCollection<Business> Businesses { get; } = new ReactiveCollection<Business>();
+        private List<UnitCategory> allUnitCategories = new List<UnitCategory>();
+        public ReactiveCollection<UnitCategory> UnitCategories { get; }
+        private List<Report> allReports = new List<Report>();
+        public ReactiveCollection<Report> Reports { get; }
+
+        public MainWindowViewModel()
+        {
+            Enumerable.Range(1, 10)
+                .Select(x => new Business
+                {
+                    BusinessId = x,
+                    Name = $"業務{x}"
+                }).ToList().ForEach(x => Businesses.Add(x));
+
+            foreach (var business in Businesses)
+            {
+                Enumerable.Range(1, 5).ToList().ForEach(x =>
+                {
+                    allUnitCategories.Add(new UnitCategory
+                    {
+                        Name = $"分類{x}"
+                    });
+
+                });
+            }
+        }
+    }
+
+    public class Business
+    {
+        public int BusinessId { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class UnitCategory
+    {
+        public string Name { get; set; }
+        public IList<int> ReportIds { get; } = new List<int>();
+    }
+
+    public class Report
+    {
+        public int ReportId { get; set; }
+        public string Name { get; set; }
+        public int BusinessId { get; set; }
+    }
+}
