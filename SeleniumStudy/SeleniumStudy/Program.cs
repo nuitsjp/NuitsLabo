e27357192d5ee5c079337c5311335c8ef73e70b4
Atsushi Nakamura <nuits.jp@live.jp>
2020-09-04 13:00:27 +0900

    Add SeleniumStudy.

---
+++ SeleniumStudy/SeleniumStudy/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+
+namespace SeleniumStudy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var driver = new InternetExplorerDriver();
+
+            driver.Navigate().GoToUrl("https://www.google.co.jp/");
+
+            var textBox = driver.FindElement(By.Name("q"));
+            textBox.SendKeys("Selenium");
+
+            var button = driver.FindElement(By.Name("btnK"));
+            button.Click();
+        }
+    }
+}
