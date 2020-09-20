9ea2df42a7fae0015ae58ac9ea2ad6c66fa745e5
Atsushi Nakamura <nuits.jp@live.jp>
2020-09-20 18:38:48 +0900

    Add WpfWithSimpleInjector.

---
+++ WpfWithSimpleInjector/WpfWithSimpleInjector/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace WpfWithSimpleInjector
+{
+    public class MainWindowViewModel
+    {
+        public MainWindowViewModel()
+        {
+            WriteLog("MainWindowViewModel()");
+        }
+        private void WriteLog([CallerMemberName] string callerName = null)
+        {
+            Debug.WriteLine(callerName);
+            File.AppendAllText("log.txt", $"{callerName}{Environment.NewLine}");
+        }
+
+    }
+}
---
+++ WpfWithSimpleInjector/WpfWithSimpleInjector/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Windows;
+using SimpleInjector;
+
+namespace WpfWithSimpleInjector
+{
+
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            var container = Bootstrap();
+
+            // Any additional other configuration, e.g. of your desired MVVM toolkit.
+
+            RunApplication(container);
+        }
+
+        private static Container Bootstrap()
+        {
+            // Create the container as usual.
+            var container = new Container();
+
+            // Register your types, for instance:
+
+            // Register your windows and view models:
+            container.Register<MainWindow>();
+            container.Register<MainWindowViewModel>();
+#if DEBUG
+            container.Verify();
+#else
+            container.Options.EnableAutoVerification = false;
+#endif
+
+            return container;
+        }
+
+        private static void RunApplication(Container container)
+        {
+            try
+            {
+                var app = new App();
+                //app.InitializeComponent();
+                var mainWindow = container.GetInstance<MainWindow>();
+                app.Run(mainWindow);
+                //app.Run(new MainWindow(new MainWindowViewModel()));
+            }
+            catch (Exception ex)
+            {
+                //Log the exception and exit
+            }
+        }
+    }
+}
