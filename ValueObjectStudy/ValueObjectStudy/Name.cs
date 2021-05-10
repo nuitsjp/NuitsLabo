d0531725a3afab68508c5524a36f62c164f3c762
Atsushi Nakamura <atsushi_a_nakamura@jp.ricoh.com>
2021-05-11 04:29:38 +0900

    Add ValueObjectStudy.

---
+++ ValueObjectStudy/ValueObjectStudy/Name.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ValueObjectStudy
+{
+    [ValueObject]
+    [ValueObjectMember(typeof(string), "firstName")]
+    [ValueObjectMember(typeof(string), "lastName")]
+    public partial class Name
+    {
+    }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+    public class ValueObjectAttribute : Attribute
+    {
+    }
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = true)]
+    public class ValueObjectMemberAttribute : Attribute
+    {
+        private readonly Type _type;
+        private readonly string _name;
+
+        public ValueObjectMemberAttribute(Type type, string name)
+        {
+            _type = type;
+            _name = name;
+        }
+    }
+
+}
