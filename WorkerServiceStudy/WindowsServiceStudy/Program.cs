2c5dd89f0b00eccdfd1180be0f777b24603eb6d7
Atsushi Nakamura <atsushi_a_nakamura@jp.ricoh.com>
2022-09-07 11:41:10 +0900

    add WindowsServiceStudy.

---
+++ WorkerServiceStudy/WindowsServiceStudy/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsServiceStudy
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        static void Main()
+        {
+            using (var processModule = System.Diagnostics.Process.GetCurrentProcess().MainModule)
+            {
+                Directory.SetCurrentDirectory(Path.GetDirectoryName(processModule.FileName));
+            }
+
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Service1()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ WorkerServiceStudy/WindowsServiceStudy/Service1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WindowsServiceStudy
+{
+    public partial class Service1 : ServiceBase
+    {
+        private bool _isRunning = true;
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Task.Run(() =>
+            {
+                while (_isRunning)
+                {
+                    File.AppendAllText("log.txt", $@"Worker running at: {DateTimeOffset.Now}{Environment.NewLine}");
+                    Thread.Sleep(TimeSpan.FromSeconds(1));
+                }
+            });
+        }
+
+        protected override void OnStop()
+        {
+            _isRunning = false;
+        }
+    }
+}
