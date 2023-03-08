50e4f46f828137519bfc3ce3ec66bffbba77b6ce
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2023-03-09 07:12:06 +0900

    update

---
+++ README.md
@@ -0,0 +1,2 @@
+# ClickOnce study
+ - [https://nuitsjp.github.io/NuitsLabo/WpfClickOnceStudy.application](https://nuitsjp.github.io/NuitsLabo/WpfClickOnceStudy.application)
--- WpfClickOnceStudy/WpfClickOnceStudy/MainWindowViewModel.cs
+++ WpfClickOnceStudy/WpfClickOnceStudy/MainWindowViewModel.cs
@@ -1,8 +1,16 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 
 namespace WpfClickOnceStudy;
 
 public class MainWindowViewModel
 {
-    public string Message => File.ReadAllText("Message.txt");
+    public string Message
+    {
+        get
+        {
+            File.WriteAllText("Message.txt", $"Hello, Click Once! from File. {DateTime.Now}");
+            return File.ReadAllText("Message.txt");
+        }
+    }
 }
