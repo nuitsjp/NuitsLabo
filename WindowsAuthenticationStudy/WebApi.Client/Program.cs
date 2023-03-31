22d8616e7be422d2330c521d92be072462725325
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2023-04-01 06:15:33 +0900

    Add WebAPI

---
+++ WindowsAuthenticationStudy/WebApi.Client/Program.cs
@@ -0,0 +1,4 @@
+﻿var httpClient  = new HttpClient();
+var response = await httpClient.GetAsync("https://localhost:7252/WeatherForecast");
+var content = await response.Content.ReadAsStringAsync();
+Console.WriteLine(content);
