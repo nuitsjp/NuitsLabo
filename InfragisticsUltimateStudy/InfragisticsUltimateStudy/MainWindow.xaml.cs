2ca805d703469afd9ccf56e0c3c5458786ac8d43
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2023-10-04 16:21:02 +0900

    一時保存

---
+++ InfragisticsUltimateStudy/InfragisticsUltimateStudy/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InfragisticsUltimateStudy
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public Item[] Items { get; set; } = new Item[]
+        {
+            new("あいうえおかきくけこさしすせそたちつてと"),
+            new("あいうえおかきくけこさしすせそたちつてと")
+        };
+    }
+
+    public class Item
+    {
+        public Item(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+    }
+}
