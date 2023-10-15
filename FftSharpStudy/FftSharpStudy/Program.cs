abebf907079b3013cb74b8ffa1f39dcfe4dfec81
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2023-10-15 13:05:35 +0900

    update

---
+++ FftSharpStudy/FftSharpStudy/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine(Math.Pow(12200, 2));
+
+double[] frequencies = {20, 1000, 20000};
+foreach (double f in frequencies)
+{
+    Console.WriteLine($"A({f} Hz) = {CalculateAWeighting(f)} dB");
+}
+
+static double CalculateAWeighting(double frequency)
+{
+    double f = frequency;
+    double numerator = 1.2589 * Math.Pow(f, 4);
+    double denominator = ((Math.Pow(f, 2) + Math.Pow(20.6, 2)) * Math.Sqrt((Math.Pow(f, 2) + Math.Pow(107.7, 2))*(Math.Pow(f, 2) + Math.Pow(737.9, 2))) * (Math.Pow(f, 2) + Math.Pow(12194, 2)));
+    double aWeighting = 20 * Math.Log10(numerator / denominator) + 2.00;
+    aWeighting = 20 * Math.Log10((Math.Pow(12200, 2) * Math.Pow(f, 4)) / ((Math.Pow(f, 2) + Math.Pow(20.6, 2)) *
+                                                                          (Math.Pow(f, 2) + Math.Pow(107.7, 2)) *
+                                                                          (Math.Pow(f, 2) + Math.Pow(737.9, 2)) *
+                                                                          (Math.Pow((Math.Pow(f, 2) + Math.Pow(12200, 2)), 2))));
+    return aWeighting;
+}
---
+++ SyncfusionBlazorStudy/Client/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Syncfusion.Blazor;
+using System.Globalization;
+using Microsoft.JSInterop;
+using SyncfusionBlazorStudy.Client.Shared;
+using SyncfusionBlazorStudy.Client;
+
+//Register Syncfusion license https://help.syncfusion.com/common/essential-studio/licensing/how-to-generate
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MjczMjQ3MkAzMjMzMmUzMDJlMzBPaVRyenl2bTZyOGNyZDFIMm5yK0Z4Z0lRc1RBd0ZOWmxFYzhMc1pQcGtjPQ==");
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddSyncfusionBlazor();
+builder.Services.AddSingleton(typeof(ISyncfusionStringLocalizer), typeof(SyncfusionLocalizer));
+
+// Set the default culture of the application
+CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("en-US");
+CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("en-US");
+
+// Get the modified culture from culture switcher
+var host = builder.Build();
+var jsInterop = host.Services.GetRequiredService<IJSRuntime>();
+var result = await jsInterop.InvokeAsync<string>("cultureInfo.get");
+if (result != null)
+{
+    // Set the culture from culture switcher
+    var culture = new CultureInfo(result);
+    CultureInfo.DefaultThreadCurrentCulture = culture;
+    CultureInfo.DefaultThreadCurrentUICulture = culture;
+}
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
