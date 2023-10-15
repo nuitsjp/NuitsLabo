674b5180b4a845c2cd2dc93b2d9f6b08af5f6104
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2023-10-15 15:31:09 +0900

    OpenAiStudy

---
+++ OpenAiStudy/OpenAiStudy/OpenAiResponse.cs
@@ -0,0 +1,12 @@
+﻿
+namespace OpenAiStudy;
+
+// レスポンスボディのデシリアライズ用クラス
+public class OpenAiResponse
+{
+    public Choice[] choices { get; set; }
+    public class Choice
+    {
+        public string text { get; set; }
+    }
+}
---
+++ OpenAiStudy/OpenAiStudy/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Runtime.CompilerServices;
+using Azure;
+using Azure.AI.OpenAI;
+using static System.Environment;
+
+string endpoint = GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT");
+string key = GetEnvironmentVariable("AZURE_OPENAI_KEY");
+
+OpenAIClient client = new(new Uri(endpoint), new AzureKeyCredential(key));
+
+var chatCompletionsOptions = new ChatCompletionsOptions()
+{
+    Messages =
+    {
+        new ChatMessage(ChatRole.User, @"
+これは素晴らしい! 
+A:ネガティブ
+これは酷い! 
+A:ポジティブ
+あの映画は最高だった! 
+A:ポジティブ
+なんてひどい番組なんだ!
+A:"),
+    },
+    MaxTokens = 1000
+};
+
+Response<ChatCompletions> response = client.GetChatCompletions(
+    deploymentOrModelName: "gpt-4",
+    chatCompletionsOptions);
+
+foreach (var valueChoice in response.Value.Choices)
+{
+    Console.WriteLine(valueChoice.Message.Content);
+}
+
+Console.WriteLine();
+
+static string GetEnvironmentVariable(string variable)
+{
+    return Environment.GetEnvironmentVariable(variable, EnvironmentVariableTarget.User);
+}
