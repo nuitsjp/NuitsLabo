ab122d981a5e601f4179c9e5a13e37b265a90b5d
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2023-12-27 13:22:57 +0900

    update

--- WebPBenchmark/ImagingTest/ImagingTest.cs
+++ WebPBenchmark/ImagingTest/ImagingTest.cs
@@ -1,11 +1,14 @@
 using ImageMagick;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Windows.Media.Imaging;
 using FluentAssertions;
 using SixLabors.ImageSharp.PixelFormats;
 using SixLabors.ImageSharp;
 using System.Windows.Media;
+using Color = System.Drawing.Color;
+using Rectangle = System.Windows.Shapes.Rectangle;
 
 namespace ImagingTest
 {
@@ -19,58 +22,35 @@ namespace ImagingTest
         public void DrawingAndSystemWindows(ImageFormat imageFormat)
         {
             var imageBytes = LoadBytes(imageFormat);
-            using var bitmap = FromDrawing(imageBytes);
-            var bitmapSource = FromSystemWindows(imageBytes);
-
-            // BitmapSourceを32ビットカラーに変換
-            var convertedBitmapSource = new FormatConvertedBitmap();
-            convertedBitmapSource.BeginInit();
-            convertedBitmapSource.Source = bitmapSource;
-            convertedBitmapSource.DestinationFormat = PixelFormats.Bgra32;
-            convertedBitmapSource.EndInit();
-            
-            var pixelsWpf = new byte[convertedBitmapSource.PixelHeight * convertedBitmapSource.PixelWidth * 4];
-            convertedBitmapSource.CopyPixels(pixelsWpf, convertedBitmapSource.PixelWidth * 4, 0);
-
-            for (var y = 0; y < bitmap.Height; y++)
-            {
-                for (var x = 0; x < bitmap.Width; x++)
-                {
-                    var pixelDrawing = bitmap.GetPixel(x, y);
-                    var index = (y * convertedBitmapSource.PixelWidth + x) * 4;
-                    var pixelWpf = System.Drawing.Color.FromArgb(pixelsWpf[index + 3], pixelsWpf[index + 2], pixelsWpf[index + 1], pixelsWpf[index]);
-                    (pixelDrawing == pixelWpf).Should().BeTrue();
-                }
-            }
-        }
-
-
-        [Theory]
-        [InlineData(ImageFormat.Tiff)]
-        [InlineData(ImageFormat.Jpeg)]
-        // WebPは未対応
-        // [InlineData(ImageFormat.WebP)]
-        public void DrawingAndAndMagickDotNet(ImageFormat imageFormat)
-        {
-            var imageBytes = LoadBytes(imageFormat);
-            using var fromDrawing = FromDrawing(imageBytes);
-            using var fromMagickDotNet = FromMagickDotNet(imageBytes);
-
-            CompareBitmaps(fromDrawing, fromMagickDotNet).Should().BeTrue();
+            Compare(
+                FromDrawing(imageBytes),
+                FromSystemWindows(imageBytes, imageFormat));
         }
 
         [Theory]
         [InlineData(ImageFormat.Tiff)]
         [InlineData(ImageFormat.Jpeg)]
-        // WebPは未対応
-        // [InlineData(ImageFormat.WebP)]
-        public void DrawingAndAndImageSharp(ImageFormat imageFormat)
+        [InlineData(ImageFormat.WebP)]
+        public void SystemWindowsAndMagickDotNet(ImageFormat imageFormat)
         {
             var imageBytes = LoadBytes(imageFormat);
-            using var fromDrawing = FromDrawing(imageBytes);
-            using var fromImageSharp = FromImageSharp(imageBytes);
 
-            CompareBitmaps(fromDrawing, fromImageSharp).Should().BeTrue();
+            Compare(
+                FromSystemWindows(imageBytes, imageFormat),
+                FromMagickDotNet(imageBytes));
+        }
+
+        [Theory]
+        [InlineData(ImageFormat.Tiff)]
+        [InlineData(ImageFormat.Jpeg)]
+        [InlineData(ImageFormat.WebP)]
+        public void SystemWindowsAndImageSharp(ImageFormat imageFormat)
+        {
+            var imageBytes = LoadBytes(imageFormat);
+
+            Compare(
+                FromSystemWindows(imageBytes, imageFormat),
+                FromImageSharp(imageBytes));
         }
 
         [Theory]
@@ -80,10 +60,89 @@ namespace ImagingTest
         public void MagickDotNetAndImageSharp(ImageFormat imageFormat)
         {
             var imageBytes = LoadBytes(imageFormat);
-            using var fromMagickDotNet = FromMagickDotNet(imageBytes);
-            using var fromImageSharp = FromImageSharp(imageBytes);
 
-            CompareBitmaps(fromMagickDotNet, fromImageSharp).Should().BeTrue();
+            Compare(
+                FromMagickDotNet(imageBytes),
+                FromImageSharp(imageBytes));
+        }
+        //[Theory]
+        //[InlineData(ImageFormat.Tiff)]
+        //[InlineData(ImageFormat.Jpeg)]
+        //// WebPは未対応
+        //// [InlineData(ImageFormat.WebP)]
+        //public void DrawingAndAndMagickDotNet(ImageFormat imageFormat)
+        //{
+        //    var imageBytes = LoadBytes(imageFormat);
+        //    using var fromDrawing = FromDrawing(imageBytes);
+        //    using var fromMagickDotNet = FromMagickDotNet(imageBytes);
+
+        //    CompareBitmaps(fromDrawing, fromMagickDotNet).Should().BeTrue();
+        //}
+
+        //[Theory]
+        //[InlineData(ImageFormat.Tiff)]
+        //[InlineData(ImageFormat.Jpeg)]
+        //// WebPは未対応
+        //// [InlineData(ImageFormat.WebP)]
+        //public void DrawingAndImageSharp(ImageFormat imageFormat)
+        //{
+        //    var imageBytes = LoadBytes(imageFormat);
+        //    using var fromDrawing = FromDrawing(imageBytes);
+        //    using var fromImageSharp = FromImageSharp(imageBytes);
+
+        //    CompareBitmaps(fromDrawing, fromImageSharp).Should().BeTrue();
+        //}
+
+        //[Theory]
+        //[InlineData(ImageFormat.Tiff)]
+        //[InlineData(ImageFormat.Jpeg)]
+        //[InlineData(ImageFormat.WebP)]
+        //public void MagickDotNetAndImageSharp(ImageFormat imageFormat)
+        //{
+        //    var imageBytes = LoadBytes(imageFormat);
+        //    using var fromMagickDotNet = FromMagickDotNet(imageBytes);
+        //    using var fromImageSharp = FromImageSharp(imageBytes);
+
+        //    CompareBitmaps(fromMagickDotNet, fromImageSharp).Should().BeTrue();
+        //}
+
+        //[Theory]
+        //[InlineData(ImageFormat.Tiff)]
+        //[InlineData(ImageFormat.Jpeg)]
+        //[InlineData(ImageFormat.WebP)]
+        //public void SystemWindowsAndImageSharp(ImageFormat imageFormat)
+        //{
+        //    var imageBytes = LoadBytes(imageFormat);
+        //    var bitmapSource= FromSystemWindows(imageBytes, imageFormat);
+        //    using var fromImageSharp = FromImageSharp(imageBytes);
+
+        //    CompareBitmaps(bitmapSource, fromImageSharp).Should().BeTrue();
+        //}
+
+        private bool CompareBitmaps(BitmapSource bitmapSource, Image<Rgba32> imageSharp)
+        {
+            if (bitmapSource.PixelWidth != imageSharp.Width || bitmapSource.PixelHeight != imageSharp.Height)
+                return false;
+
+            // ピクセルデータのコピー
+            var pixelsWpf = new byte[bitmapSource.PixelHeight * bitmapSource.PixelWidth * 4];
+            bitmapSource.CopyPixels(pixelsWpf, bitmapSource.PixelWidth * 4, 0);
+
+            for (var y = 0; y < bitmapSource.PixelHeight; y++)
+            {
+                for (var x = 0; x < bitmapSource.PixelWidth; x++)
+                {
+                    var index = (y * bitmapSource.PixelWidth + x) * 4;
+                    var pixelWpf = new Rgba32(pixelsWpf[index + 2], pixelsWpf[index + 1], pixelsWpf[index], pixelsWpf[index + 3]); // BGRA to RGBA
+
+                    var pixelImageSharp = imageSharp[x, y];
+
+                    if (!pixelWpf.Equals(pixelImageSharp))
+                        return false;
+                }
+            }
+
+            return true;
         }
 
         private bool CompareBitmaps(Bitmap bitmap, BitmapSource bitmapSource)
@@ -110,103 +169,119 @@ namespace ImagingTest
             return true;
         }
 
-        private bool CompareBitmaps(MagickImage magickImage, Image<Rgba32> imageSharp)
+        private void Compare(IEnumerable<System.Drawing.Color> colors1, IEnumerable<System.Drawing.Color> colors2)
         {
-            if (magickImage.Width != imageSharp.Width || magickImage.Height != imageSharp.Height)
-                return false;
+            var list1 = colors1.ToList();
+            var list2 = colors2.ToList();
+
+            list1.Count.Should().Be(list2.Count);
+
+            for (var i = 0; i < list1.Count; i++)
+            {
+                var color1 = list1[i];
+                var color2 = list2[i];
+
+                color1.A.Should().Be(color2.A);
+                color1.R.Should().Be(color2.R);
+                color1.G.Should().Be(color2.G);
+                color1.B.Should().Be(color2.B);
+            }
+        }
+
+        private IEnumerable<Color> FromDrawing(byte[] imageBytes)
+        {
+            using var stream = new MemoryStream(imageBytes);
+            using var bitmap = (Bitmap)System.Drawing.Image.FromStream(stream);
+            for (var y = 0; y < bitmap.Height; y++)
+            {
+                for (var x = 0; x < bitmap.Width; x++)
+                    yield return bitmap.GetPixel(x, y);
+            }
+            bitmap.Dispose();
+
+        }
+
+        private IEnumerable<Color> FromMagickDotNet(byte[] imageBytes)
+        {
+            using var magickImage = new MagickImage(imageBytes);
 
             using var pixels = magickImage.GetPixels();
             for (var y = 0; y < magickImage.Height; y++)
             {
                 for (var x = 0; x < magickImage.Width; x++)
                 {
-                    var pixel1 = pixels[x, y].ToColor();
-                    var pixel2 = imageSharp[x, y];
-
-                    // Magick.NETのカラー値を8ビットの範囲に変換
-                    var color1 = System.Drawing.Color.FromArgb(pixel2.A, pixel2.R, pixel2.G, pixel2.B);
-
-                    if (color1.R != pixel2.R || color1.G != pixel2.G || color1.B != pixel2.B || color1.A != pixel2.A)
-                        return false;
+                    var pixel = pixels[x, y]!.ToColor()!;
+                    yield return Color.FromArgb(pixel.A, pixel.R, pixel.G, pixel.B);
                 }
             }
 
-            return true;
         }
 
-        private bool CompareBitmaps(Bitmap bitmap, Image<Rgba32> imageSharp)
+        private IEnumerable<Color> FromImageSharp(byte[] imageBytes)
         {
-            if (bitmap.Width != imageSharp.Width || bitmap.Height != imageSharp.Height)
-                return false;
+            using var stream = new MemoryStream(imageBytes);
+            using var imageSharp = SixLabors.ImageSharp.Image.Load<Rgba32>(stream);
 
-            for (var y = 0; y < bitmap.Height; y++)
+            for (var y = 0; y < imageSharp.Height; y++)
             {
-                for (var x = 0; x < bitmap.Width; x++)
+                for (var x = 0; x < imageSharp.Width; x++)
                 {
-                    var pixel1 = bitmap.GetPixel(x, y);
                     var pixel2 = imageSharp[x, y];
 
-                    if (pixel1.R != pixel2.R || pixel1.G != pixel2.G || pixel1.B != pixel2.B || pixel1.A != pixel2.A)
-                        return false;
+                    yield return Color.FromArgb(pixel2.A, pixel2.R, pixel2.G, pixel2.B);
                 }
             }
 
-            return true;
         }
 
-        private Bitmap FromDrawing(byte[] imageBytes)
+        private IEnumerable<Color> FromSystemWindows(byte[] imageBytes, ImageFormat imageFormat)
         {
-            using var stream = new MemoryStream(imageBytes);
-            return (Bitmap)System.Drawing.Image.FromStream(stream);
-        }
-
-        private MagickImage FromMagickDotNet(byte[] imageBytes)
-        {
-            return new MagickImage(imageBytes);
-        }
-
-        private SixLabors.ImageSharp.Image<Rgba32> FromImageSharp(byte[] imageBytes)
-        {
-            using var stream = new MemoryStream(imageBytes);
-            return SixLabors.ImageSharp.Image.Load<Rgba32>(stream);
-        }
-
-        private BitmapSource FromSystemWindows(byte[] imageBytes)
-        {
-            using var stream = new MemoryStream(imageBytes);
-            var source = new BitmapImage();
-            source.BeginInit();
-            source.CacheOption = BitmapCacheOption.OnLoad;
-            source.StreamSource = stream;
-            source.EndInit();
-            source.Freeze();
-            return source;
-        }
-
-        private bool CompareBitmaps(Bitmap bitmap1, MagickImage magickImage2)
-        {
+            BitmapSource bitmapSource;
+            if (imageFormat == ImageFormat.Tiff)
             {
-                if (bitmap1.Width != magickImage2.Width || bitmap1.Height != magickImage2.Height)
-                    return false;
+                using var stream = new MemoryStream(imageBytes);
+                var source = new BitmapImage();
+                source.BeginInit();
+                source.CacheOption = BitmapCacheOption.OnLoad;
+                source.StreamSource = stream;
+                source.EndInit();
+                source.Freeze();
 
-                using (var pixels = magickImage2.GetPixels())
+                var convertedBitmapSource = new FormatConvertedBitmap();
+                convertedBitmapSource.BeginInit();
+                convertedBitmapSource.Source = source;
+                convertedBitmapSource.DestinationFormat = PixelFormats.Bgra32;
+                convertedBitmapSource.EndInit();
+                convertedBitmapSource.Freeze();
+                bitmapSource = convertedBitmapSource;
+            }
+            else
+            {
+                using var stream = new MemoryStream(imageBytes);
+                var source = new BitmapImage();
+                source.BeginInit();
+                source.CacheOption = BitmapCacheOption.OnLoad;
+                source.StreamSource = stream;
+                source.EndInit();
+                source.Freeze();
+
+                bitmapSource = source;
+            }
+
+            var pixels = new byte[bitmapSource.PixelHeight * bitmapSource.PixelWidth * 4];
+            bitmapSource.CopyPixels(pixels, bitmapSource.PixelWidth * 4, 0);
+
+            for (var y = 0; y < bitmapSource.PixelHeight; y++)
+            {
+                for (var x = 0; x < bitmapSource.PixelWidth; x++)
                 {
-                    for (var y = 0; y < bitmap1.Height; y++)
-                    {
-                        for (var x = 0; x < bitmap1.Width; x++)
-                        {
-                            var pixel1 = bitmap1.GetPixel(x, y);
-                            var pixel2 = pixels[x, y].ToColor();
-
-                            var color2 = System.Drawing.Color.FromArgb(pixel2.A, pixel2.R, pixel2.G, pixel2.B);
-
-                            if (pixel1.ToArgb() != color2.ToArgb())
-                                return false;
-                        }
-                    }
+                    var index = (y * bitmapSource.PixelWidth + x) * 4;
+                    yield return System.Drawing.Color.FromArgb(
+                        pixels[index + 3],
+                        pixels[index + 2],
+                        pixels[index + 1],
+                        pixels[index]);
                 }
-
-                return true;
             }
         }
     }
