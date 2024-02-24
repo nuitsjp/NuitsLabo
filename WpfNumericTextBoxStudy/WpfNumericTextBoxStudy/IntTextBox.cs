7d46124814b79214037c7c18e0e8383cf3513f1d
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2024-02-25 07:42:55 +0900

    update

--- WpfNumericTextBoxStudy/WpfNumericTextBoxStudy/IntTextBox.cs
+++ WpfNumericTextBoxStudy/WpfNumericTextBoxStudy/IntTextBox.cs
@@ -22,43 +22,44 @@ public class IntTextBox : TextBox
     }
 
     /// <summary>
-    /// 入力されたキーによって、テキストボックスの値を変更するかどうかを判定します。
+    /// スペースキーが押された場合、イベントをキャンセルします。
     /// </summary>
     /// <param name="e"></param>
     protected override void OnPreviewKeyDown(KeyEventArgs e)
     {
         base.OnPreviewKeyDown(e);
 
-        if (IsCursorMovementKey(e.Key) || IsEditingOperationKey(e.Key))
+        // スペースキーが押された場合、イベントをキャンセルします。
+        if (e.Key == Key.Space)
         {
-            // カーソル移動系キー、編集操作系キーは処理せずにリターン
-            return;
-        }
-
-        // 数値キー、マイナスキー以外のキーが押された場合、押されたキーに対応する文字を取得します。
-        if (TryGetNumericChar(e.Key, out var keyChar))
-        {
-            // 数値キー、マイナスキーが押された場合
-            // 入力されたキーから、反映後のテキストを予測します。
-            var afterText = GetAfterText(keyChar.ToString());
-            if (!IsInt(afterText))
-            {
-                // 反映後の値がint型の入力値ではない場合、イベントをキャンセルします。
-                e.Handled = true;
-            }
-        }
-        else
-        {
-            // 数値キー、マイナスキー以外のキーが押された場合、イベントをキャンセルします。
             e.Handled = true;
         }
     }
 
     /// <summary>
-    /// ペーストされたテキストを、値の反映前に検証します。
+    /// テキストボックスに入力されたテキストを、値の反映前に検証します。
     /// </summary>
-    /// <param name="sender"></param>
     /// <param name="e"></param>
+    protected override void OnPreviewTextInput(TextCompositionEventArgs e)
+    {
+        base.OnPreviewTextInput(e);
+
+        // e.Textには、入力されたテキストが入っています。
+        // 入力されたテキストから、反映後のテキストを予測します。
+        var afterText = GetAfterText(e.Text);
+
+        if (!IsInt(afterText))
+        {
+            // 反映後の値がint型の入力値ではない場合、イベントをキャンセルします。
+            e.Handled = true;
+        }
+    }
+    
+    /// <summary>
+         /// ペーストされたテキストを、値の反映前に検証します。
+         /// </summary>
+         /// <param name="sender"></param>
+         /// <param name="e"></param>
     private void OnPaste(object sender, DataObjectPastingEventArgs e)
     {
         if (e.DataObject.GetDataPresent(typeof(string)))
@@ -80,57 +81,6 @@ public class IntTextBox : TextBox
         {
             // ペーストされたデータが文字列として取得できない場合、イベントをキャンセルします。
             e.CancelCommand();
-        }
-    }
-
-    /// <summary>
-    /// カーソル移動系キーかどうかを判定します。
-    /// </summary>
-    /// <param name="key"></param>
-    /// <returns></returns>
-    private bool IsCursorMovementKey(Key key)
-    {
-        return key == Key.Left || key == Key.Right || key == Key.Up || key == Key.Down ||
-               key == Key.Home || key == Key.End || key == Key.PageUp || key == Key.PageDown ||
-               key == Key.Tab;
-    }
-
-    /// <summary>
-    /// 編集操作系キーかどうかを判定します。
-    /// </summary>
-    /// <param name="key"></param>
-    /// <returns></returns>
-    private bool IsEditingOperationKey(Key key)
-    {
-        return key == Key.Delete || key == Key.Back;
-    }
-
-    private bool TryGetNumericChar(Key key, out char keyChar)
-    {
-        switch (key)
-        {
-            // 数値キーの場合
-            case >= Key.D0 and <= Key.D9:
-                // キーコードから対応する数字を取得
-                keyChar = (char)('0' + (int)key - (int)Key.D0);
-                return true;
-            case >= Key.NumPad0 and <= Key.NumPad9:
-                // テンキーの場合、キーコードから対応する数字を取得
-                keyChar = (char)('0' + (int)key - (int)Key.NumPad0);
-                return true;
-            default:
-                // キーが特殊文字である場合
-                switch (key)
-                {
-                    case Key.OemMinus:
-                    case Key.Subtract:
-                        keyChar = '-';
-                        return true;
-                    default:
-                        // 数値またはマイナス以外の場合はkeyCharをデフォルトのchar.MinValueのままにしてfalseを返す
-                        keyChar = char.MinValue;
-                        return false;
-                }
         }
     }
 
