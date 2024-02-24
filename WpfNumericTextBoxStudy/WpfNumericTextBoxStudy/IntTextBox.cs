c22f7e0fadcd7eaded2a70389f505449706eddaa
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2024-02-25 05:58:52 +0900

    update

--- WpfNumericTextBoxStudy/WpfNumericTextBoxStudy/IntTextBox.cs
+++ WpfNumericTextBoxStudy/WpfNumericTextBoxStudy/IntTextBox.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 
@@ -22,20 +23,34 @@ public class IntTextBox : TextBox
     }
 
     /// <summary>
-    /// テキストボックスに入力されたテキストを、値の反映前に検証します。
+    /// 入力されたキーによって、テキストボックスの値を変更するかどうかを判定します。
     /// </summary>
     /// <param name="e"></param>
-    protected override void OnPreviewTextInput(TextCompositionEventArgs e)
+    protected override void OnPreviewKeyDown(KeyEventArgs e)
     {
-        base.OnPreviewTextInput(e);
+        base.OnPreviewKeyDown(e);
 
-        // e.Textには、入力されたテキストが入っています。
-        // 入力されたテキストから、反映後のテキストを予測します。
-        var afterText = GetAfterText(e.Text);
-
-        if (!IsInt(afterText))
+        if (IsCursorMovementKey(e.Key) || IsEditingOperationKey(e.Key))
         {
-            // 反映後の値がint型の入力値ではない場合、イベントをキャンセルします。
+            // カーソル移動系キー、編集操作系キーは処理せずにリターン
+            return;
+        }
+
+        // 数値キー、マイナスキー以外のキーが押された場合、押されたキーに対応する文字を取得します。
+        if (TryGetNumericChar(e.Key, out var keyChar))
+        {
+            // 数値キー、マイナスキーが押された場合
+            // 入力されたキーから、反映後のテキストを予測します。
+            var afterText = GetAfterText(keyChar.ToString());
+            if (!IsInt(afterText))
+            {
+                // 反映後の値がint型の入力値ではない場合、イベントをキャンセルします。
+                e.Handled = true;
+            }
+        }
+        else
+        {
+            // 数値キー、マイナスキー以外のキーが押された場合、イベントをキャンセルします。
             e.Handled = true;
         }
     }
@@ -70,17 +85,73 @@ public class IntTextBox : TextBox
     }
 
     /// <summary>
+    /// カーソル移動系キーかどうかを判定します。
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns></returns>
+    private bool IsCursorMovementKey(Key key)
+    {
+        return key == Key.Left || key == Key.Right || key == Key.Up || key == Key.Down ||
+               key == Key.Home || key == Key.End || key == Key.PageUp || key == Key.PageDown ||
+               key == Key.Tab;
+    }
+
+    /// <summary>
+    /// 編集操作系キーかどうかを判定します。
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns></returns>
+    private bool IsEditingOperationKey(Key key)
+    {
+        return key == Key.Delete || key == Key.Back;
+    }
+
+    private bool TryGetNumericChar(Key key, out char keyChar)
+    {
+        switch (key)
+        {
+            // 数値キーの場合
+            case >= Key.D0 and <= Key.D9:
+                // キーコードから対応する数字を取得
+                keyChar = (char)('0' + (int)key - (int)Key.D0);
+                return true;
+            case >= Key.NumPad0 and <= Key.NumPad9:
+                // テンキーの場合、キーコードから対応する数字を取得
+                keyChar = (char)('0' + (int)key - (int)Key.NumPad0);
+                return true;
+            default:
+                // キーが特殊文字である場合
+                switch (key)
+                {
+                    case Key.OemMinus:
+                    case Key.Subtract:
+                        keyChar = '-';
+                        return true;
+                    default:
+                        // 数値またはマイナス以外の場合はkeyCharをデフォルトのchar.MinValueのままにしてfalseを返す
+                        keyChar = char.MinValue;
+                        return false;
+                }
+        }
+    }
+
+    /// <summary>
     /// テキストがint型の値かどうかを判定します。
     /// </summary>
     /// <param name="text"></param>
     /// <returns></returns>
     private bool IsInt(string text)
     {
+        if ("-".Equals(text))
+        {
+            // マイナス記号のみの場合はtrueを返す
+            return true;
+        }
+
         // まずint形にパースして確認する。
         if (int.TryParse(text, out var intValue))
         {
-            // パースに成功した場合、元の文字列とint型に変換した値が一致するかを確認する。
-            // スペースが含まれている場合（など？）は、一致しないのでfalseを返す。
+            // 01などを除外するために、パースした値を文字列に変換して、入力値と一致するかどうかを確認する。
             return string.Equals(text, intValue.ToString());
         }
         return false;
--- WpfNumericTextBoxStudy/WpfNumericTextBoxStudy/MainWindowViewModel.cs
+++ WpfNumericTextBoxStudy/WpfNumericTextBoxStudy/MainWindowViewModel.cs
@@ -9,9 +9,9 @@ public partial class MainWindowViewModel : ObservableObject
     public MainWindowViewModel()
     {
         this.ObserveProperty(x => x.Text1)
-            .Subscribe(x => Debug.WriteLine($"VM Text1 changed: {x}"));
+            .Subscribe(x => Debug.WriteLine($"VM Text1 changed: '{x}'"));
         this.ObserveProperty(x => x.Text2)
-            .Subscribe(x => Debug.WriteLine($"VM Text2 changed: {x}"));
+            .Subscribe(x => Debug.WriteLine($"VM Text2 changed: '{x}'"));
     }
 
     [ObservableProperty] private string _text1 = "";
