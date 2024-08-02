c1e59984d3aee33cef46feb1f5bc58e77efe39f9
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2024-08-03 06:02:24 +0900

    wip

--- AudioCalibrationStudy/AudioCalibrationStudy/AudioSplineCalibrator.cs
+++ AudioCalibrationStudy/AudioCalibrationStudy/AudioSplineCalibrator.cs
@@ -1,13 +1,6 @@
 ﻿namespace AudioCalibrationStudy;
 
 /// <summary>
-/// キャリブレーションポイントを表すレコード
-/// </summary>
-/// <param name="Decibels">デシベル値</param>
-/// <param name="Amplitude">振幅</param>
-public record CalibrationPoint(double Decibels, double Amplitude);
-
-/// <summary>
 /// スプライン補間を使用してオーディオのキャリブレーションを行うクラス
 /// </summary>
 public class AudioSplineCalibrator
---
+++ AudioCalibrationStudy/AudioCalibrationStudy/CalibrationPoint.cs
@@ -0,0 +1,8 @@
+﻿namespace AudioCalibrationStudy;
+
+/// <summary>
+/// キャリブレーションポイントを表すレコード
+/// </summary>
+/// <param name="Decibels">デシベル値</param>
+/// <param name="Amplitude">振幅</param>
+public record CalibrationPoint(double Decibels, double Amplitude);
