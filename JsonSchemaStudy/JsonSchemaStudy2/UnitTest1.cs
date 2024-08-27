ab6783880739d693af61b44ae44258c5ba388141
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2024-08-27 17:10:57 +0900

    wip

---
+++ JsonSchemaStudy/JsonSchemaStudy2/UnitTest1.cs
@@ -0,0 +1,62 @@
+using FluentAssertions;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+
+namespace JsonSchemaStudy2
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData("valid-01.json")]
+        [InlineData("valid-02.json")]
+        [InlineData("valid-03.json")]
+        [InlineData("valid-04.json")]
+        [InlineData("valid-05.json")]
+        [InlineData("valid-06.json")]
+        [InlineData("valid-07.json")]
+        [InlineData("valid-08.json")]
+        [InlineData("valid-09.json")]
+        public async Task Valid(string jsonFile)
+        {
+            // スキーマファイルを読み込む
+            var schemaJson = await File.ReadAllTextAsync("ridw-image-import-unit-schema-v1.0.0.json");
+            var schema = JSchema.Parse(schemaJson);
+
+            // JSONファイルの内容を読み込む
+            var jsonData = await File.ReadAllTextAsync(jsonFile);
+            var jsonObject = JObject.Parse(jsonData);
+
+            // JSONのバリデーション
+            jsonObject.IsValid(schema, out IList<ValidationError> errors)
+                .Should()
+                .BeTrue();
+        }
+
+        [Theory]
+        [InlineData("error-01.json")]
+        [InlineData("error-02.json")]
+        [InlineData("error-03.json")]
+        [InlineData("error-04.json")]
+        [InlineData("error-05.json")]
+        [InlineData("error-06.json")]
+        [InlineData("error-07.json")]
+        [InlineData("error-08.json")]
+        [InlineData("error-09.json")]
+        public async Task Error(string jsonFile)
+        {
+            // スキーマファイルを読み込む
+            var schemaJson = await File.ReadAllTextAsync("ridw-image-import-unit-schema-v1.0.0.json");
+            var schema = JSchema.Parse(schemaJson);
+
+            // JSONファイルの内容を読み込む
+            var jsonData = await File.ReadAllTextAsync(jsonFile);
+            var jsonObject = JObject.Parse(jsonData);
+
+            // JSONのバリデーション
+            jsonObject.IsValid(schema, out IList<ValidationError> errors)
+                .Should()
+                .BeFalse();
+        }
+
+    }
+}
