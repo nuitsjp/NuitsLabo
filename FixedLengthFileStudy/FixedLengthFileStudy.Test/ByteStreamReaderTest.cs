820f8f9197c5005ed55518e0a66e447e82799fb8
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2024-12-18 04:03:27 +0900

    wip

--- FixedLengthFileStudy/FixedLengthFileStudy.Test/ByteStreamReaderTest.cs
+++ FixedLengthFileStudy/FixedLengthFileStudy.Test/ByteStreamReaderTest.cs
@@ -10,28 +10,6 @@ public class ByteStreamReaderTest
         Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
     }
 
-    //[Theory]
-    //[InlineData("Shift_JIS", "\r")]
-    //[InlineData("UTF-8", "\r")]
-    //[InlineData("Shift_JIS", "\n")]
-    //[InlineData("UTF-8", "\n")]
-    //[InlineData("Shift_JIS", "\r\n")]
-    //[InlineData("UTF-8", "\r\n")]
-    //public void ReadTest(string encodingName, string newline)
-    //{
-    //    // Arrange
-    //    var encoding = Encoding.GetEncoding(encodingName);
-    //    var content = "123" + newline + "456" + newline + "789";
-    //    var stream = new MemoryStream(encoding.GetBytes(content));
-    //    using var reader = new ByteStreamReader(stream, encoding);
-
-    //    // Act
-    //    reader.ReadLine().Should().Be("123");
-    //    reader.ReadLine().Should().Be("456");
-    //    reader.ReadLine().Should().Be("789");
-    //    reader.ReadLine().Should().BeNull();
-    //}
-
     [Theory]
     [InlineData("Shift_JIS", "\r", null)]
     [InlineData("UTF-8", "\r", null)]
@@ -45,7 +23,7 @@ public class ByteStreamReaderTest
     [InlineData("UTF-8", "\n", 8000)]
     [InlineData("Shift_JIS", "\r\n", 8000)]
     [InlineData("UTF-8", "\r\n", 8000)]
-    public void ReadByteArrayTest(string encodingName, string newline, int? bufferSize)
+    public void ReadLine(string encodingName, string newline, int? bufferSize)
     {
         // Arrange
         var encoding = Encoding.GetEncoding(encodingName);
