7576ea7d054e508551d9a15bb8dcf5136ba64513
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2024-12-18 05:49:41 +0900

    wip

---
+++ FixedLengthFileStudy/FixedLengthFileStudy.Benchmark/ByteStreamReaderBenchmark.cs
@@ -0,0 +1,74 @@
+﻿using BenchmarkDotNet.Attributes;
+using System.Text;
+using V001 = FixedLengthFileStudy.Benchmark.V001;
+using V002 = FixedLengthFileStudy.Benchmark.V002;
+
+namespace FixedLengthFileStudy.Benchmark;
+
+[DryJob]
+public class ByteStreamReaderBenchmark : IDisposable
+{
+    private static readonly int _lineCount = 10000;
+    private static readonly byte[] _content;
+
+    static ByteStreamReaderBenchmark()
+    {
+        StringBuilder builder = new();
+        for (var i = 0; i < _lineCount; i++)
+        {
+            builder.Append(new string('あ', 8000));
+            builder.Append("\r\n");
+        }
+        _content = Encoding.UTF8.GetBytes(builder.ToString());
+    }
+
+    [Benchmark]
+    public void V001()
+    {
+        using var reader = new V001.ByteStreamReader(new MemoryStream(_content));
+
+        for (var i = 0; i < _lineCount; i++)
+        {
+            reader.ReadLine();
+        }
+    }
+
+    [Benchmark]
+    public void V001_10K()
+    {
+        using var reader = new V001.ByteStreamReader(new MemoryStream(_content), 10_000);
+
+        for (var i = 0; i < _lineCount; i++)
+        {
+            reader.ReadLine();
+        }
+    }
+
+
+    [Benchmark]
+    public void V002()
+    {
+        using var reader = new V002.ByteStreamReader(new MemoryStream(_content));
+
+        for (var i = 0; i < _lineCount; i++)
+        {
+            reader.ReadLine();
+        }
+    }
+
+    [Benchmark]
+    public void V002_10K()
+    {
+        using var reader = new V002.ByteStreamReader(new MemoryStream(_content), 10_000);
+
+        for (var i = 0; i < _lineCount; i++)
+        {
+            reader.ReadLine();
+        }
+    }
+
+    public void Dispose()
+    {
+        // TODO release managed resources here
+    }
+}
---
+++ FixedLengthFileStudy/FixedLengthFileStudy.Benchmark/V001/ValueByteArrayBuilder.cs
@@ -0,0 +1,81 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+#nullable enable
+
+using System.Buffers;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace FixedLengthFileStudy.Benchmark.V001;
+
+internal ref struct ValueByteArrayBuilder(Span<byte> initialBuffer)
+{
+    private byte[]? _arrayToReturnToPool = null;
+    private Span<byte> _bytes = initialBuffer;
+    private int _pos = 0;
+
+    public bool IsEmpty => _pos == 0;
+
+    public void Clear() => _pos = 0;
+
+    public ReadOnlySpan<byte> AsSpan() => _bytes.Slice(0, _pos);
+
+    public void Append(scoped ReadOnlySpan<byte> value)
+    {
+        if (_pos > _bytes.Length - value.Length)
+        {
+            Grow(value.Length);
+        }
+
+        value.CopyTo(_bytes.Slice(_pos));
+        _pos += value.Length;
+    }
+
+    /// <summary>
+    /// Resize the internal buffer either by doubling current buffer size or
+    /// by adding <paramref name="additionalCapacityBeyondPos"/> to
+    /// <see cref="_pos"/> whichever is greater.
+    /// </summary>
+    /// <param name="additionalCapacityBeyondPos">
+    /// Number of chars requested beyond current position.
+    /// </param>
+    [MethodImpl(MethodImplOptions.NoInlining)]
+    private void Grow(int additionalCapacityBeyondPos)
+    {
+        Debug.Assert(additionalCapacityBeyondPos > 0);
+        Debug.Assert(_pos > _bytes.Length - additionalCapacityBeyondPos, "Grow called incorrectly, no resize is needed.");
+
+        const uint arrayMaxLength = 0x7FFFFFC7; // same as Array.MaxLength
+
+        // Increase to at least the required size (_pos + additionalCapacityBeyondPos), but try
+        // to double the size if possible, bounding the doubling to not go beyond the max array length.
+        var newCapacity = (int)Math.Max(
+            (uint)(_pos + additionalCapacityBeyondPos),
+            Math.Min((uint)_bytes.Length * 2, arrayMaxLength));
+
+        // Make sure to let Rent throw an exception if the caller has a bug and the desired capacity is negative.
+        // This could also go negative if the actual required length wraps around.
+        var poolArray = ArrayPool<byte>.Shared.Rent(newCapacity);
+
+        _bytes.Slice(0, _pos).CopyTo(poolArray);
+
+        var toReturn = _arrayToReturnToPool;
+        _bytes = _arrayToReturnToPool = poolArray;
+        if (toReturn != null)
+        {
+            ArrayPool<byte>.Shared.Return(toReturn);
+        }
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Dispose()
+    {
+        var toReturn = _arrayToReturnToPool;
+        this = default; // for safety, to avoid using pooled array if this instance is erroneously appended to again
+        if (toReturn != null)
+        {
+            ArrayPool<byte>.Shared.Return(toReturn);
+        }
+    }
+}
