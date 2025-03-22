e7f051ba42cea5d6be7c8f3727e30a2d2552d774
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2025-03-23 04:37:24 +0900

    wip

--- ImageLibBenchmarks/ImagingLib/SystemDrawingExtensions.cs
+++ ImageLibBenchmarks/ImagingLib/SystemDrawingExtensions.cs
@@ -69,45 +69,8 @@ public static class SystemDrawingExtensions
                     }
                 }
 
-                // 画像全体のピクセル数
-                var totalPixels = srcBitmapData.Width * srcBitmapData.Height;
-                float sumOfHistogram = 0;
 
-                // ヒストグラムの合計を計算
-                for (var i = 0; i < 256; i++)
-                {
-                    sumOfHistogram += i * histogram[i];
-                }
-
-                float sumB = 0; // 背景のピクセルの合計
-                var weightBackground = 0; // 背景の重み
-
-                float maxVariance = 0; // 最大分散
-                var optimalThreshold = 0; // 最適なしきい値
-
-                // 各階調に対して最適なしきい値を計算
-                for (var i = 0; i < 256; i++)
-                {
-                    weightBackground += histogram[i];
-                    if (weightBackground == 0) continue;
-
-                    var weightForeground = totalPixels - weightBackground; // 前景の重み
-                    if (weightForeground == 0) break;
-
-                    sumB += i * histogram[i];
-                    var meanBackground = sumB / weightBackground;
-                    var meanForeground = (sumOfHistogram - sumB) / weightForeground;
-
-                    // 背景と前景の分散を計算
-                    var varianceBetween = weightBackground * (float)weightForeground * (meanBackground - meanForeground) * (meanBackground - meanForeground);
-                    if (varianceBetween > maxVariance)
-                    {
-                        maxVariance = varianceBetween;
-                        optimalThreshold = i;
-                    }
-                }
-
-                return (int)(optimalThreshold / 256f * 100);
+                return histogram.OptimalThreshold(srcBitmapData.Width, srcBitmapData.Height);
             }
             finally
             {
