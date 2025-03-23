9f71b0084a9f71cab9b78da82c025e3a2ce5fae4
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2025-03-23 20:14:36 +0900

    wip

--- ImageLibBenchmarks/Benchmarks/BenchmarkBase.cs
+++ ImageLibBenchmarks/Benchmarks/BenchmarkBase.cs
@@ -10,10 +10,6 @@ public class BenchmarkBase
     private static readonly byte[] Jpeg = File.ReadAllBytes("Color.jpg");
     private static readonly byte[] Tiff = File.ReadAllBytes("Color.tiff");
 
-    private static readonly BitmapSource WebPBitmapSource = CreateBitmapSource(WebP);
-    private static readonly BitmapSource JpegBitmapSource = CreateBitmapSource(Jpeg);
-    private static readonly BitmapSource TiffBitmapSource = CreateBitmapSource(Tiff);
-
     [Params("Jpeg", "WebP", "Tiff")]
     public virtual string Format { get; set; } = "WebP";
 
@@ -27,27 +23,4 @@ public class BenchmarkBase
             "Tiff" => Tiff,
             _ => throw new NotSupportedException()
         };
-
-    protected BitmapSource BitmapSource =>
-        Format switch
-        {
-            "Jpeg" => JpegBitmapSource,
-            "WebP" => WebPBitmapSource,
-            "Tiff" => TiffBitmapSource,
-            _ => throw new NotSupportedException()
-        };
-
-    /// <summary>
-    /// バイト列からBitmapSourceを生成します。
-    /// </summary>
-    private static BitmapSource CreateBitmapSource(byte[] data)
-    {
-        var bitmapImage = new BitmapImage();
-        bitmapImage.BeginInit();
-        bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
-        bitmapImage.StreamSource = new MemoryStream(data);
-        bitmapImage.EndInit();
-        bitmapImage.Freeze();
-        return bitmapImage;
-    }
 }
