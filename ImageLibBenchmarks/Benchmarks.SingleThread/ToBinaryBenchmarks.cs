f659479074afaf2475ff4bcf5af9e0f81dea2fa9
Atsushi Nakamura <16175660+nuitsjp@users.noreply.github.com>
2025-03-23 09:52:51 +0900

    .gitignoreにAsposeのライセンスファイルを追加し、ToBinaryBenchmarksクラスにLibTiffメソッドを追加

--- ImageLibBenchmarks/Benchmarks.SingleThread/ToBinaryBenchmarks.cs
+++ ImageLibBenchmarks/Benchmarks.SingleThread/ToBinaryBenchmarks.cs
@@ -7,7 +7,7 @@ using ImagingLib;
 namespace Benchmarks.SingleThread;
 
 [MemoryDiagnoser]
-//[SimpleJob(launchCount: 1, warmupCount: 1, iterationCount: 1, invocationCount: 1)]
+// [SimpleJob(launchCount: 1, warmupCount: 1, iterationCount: 1, invocationCount: 1)]
 [SimpleJob]
 public class ToBinaryBenchmarks : BenchmarkBase
 {
@@ -52,6 +52,14 @@ public class ToBinaryBenchmarks : BenchmarkBase
     public void SkiaSharp()
     {
         using var bin = SkiaSharpExtensions.ToBinary(Data);
+    }
+
+
+
+    [Benchmark]
+    public void LibTiff()
+    {
+        using var bin = LibTiffExtensions.ToBinary(Data);
     }
 
 #if NET8_0_OR_GREATER
